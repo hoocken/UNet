cb7fdef350fa0568c42fdcc15cca2e2e938a9d4a
go34dij <anthony.tang@tum.de>
2026-06-08 19:53:55 +0200

    Clean up

--- train.py
+++ train.py
@@ -13,7 +13,6 @@ from solver import Solver
 @hydra.main(version_base=None, config_path='config', config_name='config')
 def main(config: DictConfig):
     config_data = config.data
-    config_train = config.train
     
     train_ld, valid_ld = build_loader(
         config_data.filepath,
@@ -34,11 +33,3 @@ def main(config: DictConfig):
 
 if __name__ == "__main__":
     main()
-    # labels_path = "/media/data/student/paxraypp/labels_unpacked/labels_converted/"
-    # name = "RSNAPE_56314d0397f4_ba8be36234c2_lateral.npy"
-    # arr = np.load(labels_path + name)
-    # print(arr.shape)
-    # plt.imsave("label_plot.png", arr[4])
-
-    # 3. Save as image
-    # plt.savefig("label_plot.png", dpi=300, bbox_inches="tight")
