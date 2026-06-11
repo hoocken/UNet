a2e7541a55897b58e1122938c5d2239e8d59b13b
go34dij <anthony.tang@tum.de>
2026-06-11 21:15:26 +0200

    Added channels

--- train.py
+++ train.py
@@ -13,14 +13,18 @@ from solver import Solver
 @hydra.main(version_base=None, config_path='config', config_name='config')
 def main(config: DictConfig):
     config_data = config.data
+    config_train = config.train
     
     train_ld, valid_ld = build_loader(
         config_data.filepath,
         config_data.image_dir,
         config_data.labels_dir,
         batch_size=config_data.batch_size,
-        # limit_files=50,
+        channels=config_data.channels
+        # limit_files=2,
     )
+
+    config_train.num_classes = len(config_data.channels) + 1 if not config_data.channels is None else config_train.num_classes
 
     solver = Solver(
         train_ld,
