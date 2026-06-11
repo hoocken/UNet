2296986e1e03b3e27d9dad38be4ae9ec7c6a0f9d
go34dij <anthony.tang@tum.de>
2026-06-11 21:15:11 +0200

    Updated UNet to use new layers

--- model/unet.py
+++ model/unet.py
@@ -7,22 +7,26 @@ class UNet(nn.Module):
     def __init__(self, num_classes):
         super(UNet, self).__init__()
         self.down_convs = nn.ModuleList([
-            DownConv(1, 64),
-            DownConv(64, 128),
-            DownConv(128, 256),
-            DownConv(256, 512),
+            DownConv(1, 32), # 256
+            DownConv(32, 64), # 128
+            DownConv(64, 128), # 64
+            DownConv(128, 256), # 32
+            DownConv(256, 480), # 16
+            DownConv(480, 480), # 8
         ])
         
-        self.conv = Conv(512, 1024, 2)
+        self.conv = Conv(480, 480, 2)
 
         self.up_convs =  nn.ModuleList([
-            UpConv(1024, 512),
-            UpConv(512, 256),
-            UpConv(256, 128),
-            UpConv(128, 64),
+            UpConv(480, 480, 480),
+            UpConv(480, 480, 480),
+            UpConv(480, 256, 256),
+            UpConv(256, 128, 128),
+            UpConv(128, 64, 64),
+            UpConv(64, 32, 32),
         ])
 
-        self.out_conv = nn.Conv2d(64, num_classes, kernel_size=1)
+        self.out_conv = nn.Conv2d(32, num_classes, kernel_size=1)
         self.activation = nn.Sigmoid()
 
     def forward(self, x):
