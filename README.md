c4d8329fca1022e53387fc9898bcba5663504d78
go34dij <anthony.tang@tum.de>
2026-08-13 12:06:17 +0200

    Finished run_segmentation.py

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # UNet Implementation for Torso Segmentation
 The U-Net architecture is inspired from this [repo](https://github.com/milesial/Pytorch-UNet) and some adjustments are from the [nnU-Net](https://www.nature.com/articles/s41592-020-01008-z) paper. Namely, this uses instance normalization as the batch size is very small. The loss used is also taken from the nnU-Net paper, however this used generalized dice loss instead of the normal one.
+
+## Usage
+To run the segmentation, simply do
+```
+python run_segmentation.py run.input_image=<INPUT_FILE>
+```
+
+or set your own filepaths in `config/config.yaml`.
--- run_segmentation.py
+++ run_segmentation.py
@@ -1,4 +1,3 @@
-from data.transform import GaussianNoise
 from model.unet import UNet
 from omegaconf import DictConfig
 import hydra
@@ -9,17 +8,18 @@ from PIL import Image
 from torchvision.transforms.functional import pil_to_tensor
 
 
-from data.dataset import build_loader
-from solver import Solver
-
 def load_unet(weights: str) -> UNet:
+    checkpoint = torch.load(weights, weights_only=True)
     state_dict = checkpoint['unet_state_dict']
-    num_classes = state_dict.out_conv
-    num_layers = len(state_dict.down_conv) + 1
-    max_channels = 512 # TODO: Get channel number of deepest conv layer.
+
+    num_classes = state_dict['out_conv.weight'].shape[0]
+    down_convs = [ down.split('.')[1] for down in state_dict if 'down_convs' in down ]
+    num_layers = int(down_convs[-1]) + 2
+
+    deepest_conv = [ conv for conv in state_dict if 'conv.conv' in conv and 'weight' in conv ]
+    max_channels = state_dict[deepest_conv[-1]].shape[0]
 
     unet = UNet(num_classes, num_layers, max_channels, 0)
-    checkpoint = torch.load(weights, weights_only=True)
 
     # Remove deep supervision heads (if available)
     module_prefix = 'deep_supervision_heads'
@@ -34,55 +34,34 @@ def load_unet(weights: str) -> UNet:
 
 """
 Mapping:
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] # spine, scapula left, scapula right, sternum, clavicle left, clavicle right, lung_lower_lobe_left, lung_upper_lobe_left, lung_lower_lobe_right, lung_middle_lobe_right, lung_upper_lobe_right, heart
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] # spine, scapula left, scapula right, sternum, clavicle left, clavicle right, lung_lower_lobe_left, lung_upper_lobe_left, lung_lower_lobe_right, lung_middle_lobe_right, lung_upper_lobe_right, heart
 """
 @hydra.main(version_base=None, config_path='config', config_name='config')
 def main(config: DictConfig):
     config = config.run
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # lung_heart_unet = UNet(config.num_classes, config.num_layers, config.max_channels, 0).to(device)
-    # lung_heart_checkpoint = torch.load(config.weights, weights_only=True)
-    # lung_heart_state_dict = lung_heart_checkpoint['unet_state_dict']
-
-    # # Remove deep supervision heads (if available)
-    # module_prefix = 'deep_supervision_heads'
-    # keys_to_remove = [k for k in lung_heart_state_dict.keys() if k.startswith(module_prefix)]
-
-    # for k in keys_to_remove:
-    #     del lung_heart_state_dict[k]
-
-    # lung_heart_unet.load_state_dict(lung_heart_state_dict)
     lung_heart_unet = load_unet(config.lung_heart_weights).to(device)
     bones_unet = load_unet(config.bones_weights).to(device)
     
-    labels_path = "/media/data/student/paxraypp/labels_unpacked/labels/"
-    images_path = "/media/data/student/paxraypp/paxray_images_unfiltered/images_patlas/"
-    # images_path = "./"
-    # name = "image_00000copy"
-    name="RibFrac_056_frontal"
-
-    image = Image.open(images_path + name + ".png").convert('L')
+    image = Image.open(config.input_image).convert('L')
     image = pil_to_tensor(image).to(dtype=torch.float32, device=device) / 255
-    image = image.unsqueeze(0)
+    image = image.unsqueeze(0).to(device)
 
-    pred = np.load(labels_path + name + ".npy")
-    # transform = GaussianNoise()
-    # image, _ = transform(image[None, :, :, :], torch.zeros_like(image[None, :, :, :]))
-    # plt.imsave("img_plot.png", image[0, 0].detach().cpu(), cmap='gray')
+    if config.label_image:
+        # TODO: Add measurement of dice scores (for evaluation)
+        label = np.load(config.label_image)
+
     lung_heart_result = lung_heart_unet(image)
     bones_result = bones_unet(image)
 
-    result = torch.cat([bones_result[0], lung_heart_result[0]]) # Concat the channels
+    result = torch.cat([bones_result[0][1:], lung_heart_result[0][1:]]) # Concat the channels
 
     np.save(config.output_folder + '/segmentation_result.npy', result.detach().cpu())
-
-    # plt.imsave("pred_plot.png", res[3] > 0.5)
-    # plt.imsave("label_plot.png", pred[129])
-
-
-
+    # plt.imsave("result_plot.png", result.cpu()[13] > 0.5)
+    # plt.imsave("label_plot.png", label[18])
 
 if __name__ == "__main__":
     # TODO: Add running from the CLI
     main()
+    # print('hi')
